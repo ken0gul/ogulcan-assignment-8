14c31e89a43898d02981ff51b118917c2eca93c7
TheJohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-10-29 13:17:26 -0400

    Remove the whitespace in the code

--- src/com/coderscampus/assignment/Main.java
+++ src/com/coderscampus/assignment/Main.java
@@ -16,63 +16,27 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		Assignment8 assignment = new Assignment8();
-
+		ProcessData data = new ProcessData();
 		// To fetch data use newFixedThreadPool
 		ExecutorService service = Executors.newFixedThreadPool(1000);
 
 		// Only to print the exptected output in a different thread
 		ExecutorService service2 = Executors.newSingleThreadExecutor();
 
-		
-		
 		List<CompletableFuture<List<Integer>>> list = new ArrayList<>();
-		for (int i = 0; i < 1000; i++) {
-			CompletableFuture<List<Integer>> numbersList = CompletableFuture.supplyAsync(() -> assignment.getNumbers(),
-					service);
-			list.add(numbersList);
+		data.fetch(service, list, assignment);
 
-		}
-
-		// Keep the thread alive
-		while (list.stream().filter(CompletableFuture::isDone).count() < 1000) {
-		}
-
-		
-		
 		List<Integer> newList = new ArrayList<>();
-		list.forEach(item -> {
-			// Add List<Integer>'s if CompletableFuture is done
-			if (item.isDone()) {
 
-				try {
-					newList.addAll(item.get());
+		// Get the value without blocking the main thread and store it into a new List
+		data.addToList(list, newList);
 
-				} catch (InterruptedException | ExecutionException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		// Sort them to find the occurence of each unique number in the List
+		Collections.sort(newList);
 
-			}
+		// Print the expected output using a different single thread
+		data.printResult(service2, newList);
 
-			
-			
-			// Sort them to find the occurence of each unique number in the List
-			Collections.sort(newList);
-			
-			// Run it in a different thread but a single one because we only need one transaction
-			service2.submit(() -> {
-				Map<String, Long> map = newList.stream()
-						.collect(Collectors.groupingBy(i -> i.toString(), Collectors.counting()));
-				Set<Entry<String, Long>> entrySet = map.entrySet();
-				System.out.println(entrySet);
-				
-				// shutdown the thread after 1 transaction
-				service2.shutdownNow();
-
-			});
-
-		});
-
-	}
+	};
 
 }
--- src/com/coderscampus/assignment/ProcessData.java
+++ src/com/coderscampus/assignment/ProcessData.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 public class ProcessData {
 
 	public void fetch(ExecutorService service, List<CompletableFuture<List<Integer>>> list, Assignment8 assignment) {
-		
+
 		for (int i = 0; i < 1000; i++) {
 			CompletableFuture<List<Integer>> numbersList = CompletableFuture.supplyAsync(() -> assignment.getNumbers(),
 					service);
@@ -24,10 +24,9 @@ public class ProcessData {
 		while (list.stream().filter(CompletableFuture::isDone).count() < 1000) {
 		}
 	}
-	
-	
+
 	public void addToList(List<CompletableFuture<List<Integer>>> list, List<Integer> newList) {
-		
+
 		list.forEach(item -> {
 			// Add List<Integer>'s if CompletableFuture is done
 			if (item.isDone()) {
@@ -42,43 +41,23 @@ public class ProcessData {
 
 			}
 
-	});
-	
-	
-	
-	
-}
-	
-	public void printResult(ExecutorService service, List<Integer> list ) {
-		// Run it in a different thread but a single one because we only need one transaction
-					service.submit(() -> {
-						Map<String, Long> map = list.stream()
-								.collect(Collectors.groupingBy(i -> i.toString(), Collectors.counting()));
-						Set<Entry<String, Long>> entrySet = map.entrySet();
-						System.out.println(entrySet);
-						
-						// shutdown the thread after 1 transaction
-						service.shutdownNow();
+		});
 
-					});
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public void printResult(ExecutorService service, List<Integer> list) {
+		// Run it in a different thread but a single one because we only need one
+		// transaction
+		service.submit(() -> {
+			Map<String, Long> map = list.stream()
+					.collect(Collectors.groupingBy(i -> i.toString(), Collectors.counting()));
+			Set<Entry<String, Long>> entrySet = map.entrySet();
+			System.out.println(entrySet);
+
+			// shutdown the thread after 1 transaction
+			service.shutdownNow();
+
+		});
+	}
+
 }
-	
