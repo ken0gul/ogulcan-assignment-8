1ee38505c93c99c113b553d043c0b3a2097c4188
TheJohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-10-29 12:46:35 -0400

    Put everything in main and  make it work first

--- src/com/coderscampus/assignment/Main.java
+++ src/com/coderscampus/assignment/Main.java
@@ -1,16 +1,78 @@
 package com.coderscampus.assignment;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		Assignment8 assignment = new Assignment8();
-		List<Integer> list = assignment.getNumbers();
-		for(Integer i : list) {
-			System.out.println(i);
+
+		// To fetch data use newFixedThreadPool
+		ExecutorService service = Executors.newFixedThreadPool(1000);
+
+		// Only to print the exptected output in a different thread
+		ExecutorService service2 = Executors.newSingleThreadExecutor();
+
+		
+		
+		List<CompletableFuture<List<Integer>>> list = new ArrayList<>();
+		for (int i = 0; i < 1000; i++) {
+			CompletableFuture<List<Integer>> numbersList = CompletableFuture.supplyAsync(() -> assignment.getNumbers(),
+					service);
+			list.add(numbersList);
+
 		}
+
+		// Keep the thread alive
+		while (list.stream().filter(CompletableFuture::isDone).count() < 1000) {
+		}
+
+		
+		
+		List<Integer> newList = new ArrayList<>();
+		list.forEach(item -> {
+			// Add List<Integer>'s if CompletableFuture is done
+			if (item.isDone()) {
+
+				try {
+					newList.addAll(item.get());
+
+				} catch (InterruptedException | ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+			
+			
+			// Sort them to find the occurence of each unique number in the List
+			Collections.sort(newList);
+			
+			// Run it in a different thread but a single one because we only need one transaction
+			service2.submit(() -> {
+				Map<String, Long> map = newList.stream()
+						.collect(Collectors.groupingBy(i -> i.toString(), Collectors.counting()));
+				Set<Entry<String, Long>> entrySet = map.entrySet();
+				System.out.println(entrySet);
+				
+				// shutdown the thread after 1 transaction
+				service2.shutdownNow();
+
+			});
+
+		});
+
 	}
 
 }
